32020bd4ea86bcd4b6411e24c3bd0e0eae24d94f
nestor-labiuk <nestorlabiuk@gmail.com>
2024-03-25 22:07:47 -0300

    Agrega codigos http  y mensajes  a los controladores de usuarios para mejorar las respuestas

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -3,7 +3,11 @@ import User from '../models/User.js'
 export const getAllUsers = async (req, res, next ) => {
     try {
         const users = await User.find()
-        res.json(users)
+        if (!users) {
+            return res.status(204).json({ message: 'List is empty' })
+        }
+        res.status(200).json({ message: 'You got the list of users', users })
+    
     } catch (err) {
         res.json(err)
         next(err)
@@ -14,7 +18,11 @@ export const getUser = async (req, res, next) => {
     try {
         const { id } = req.params
         const user = await User.findById(id)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
         res.json(user)
+    
     } catch (err) {
         res.json(err)
         next(err)
@@ -26,7 +34,8 @@ export const createUser = async (req, res, next ) => {
         const { name, email, password } = req.body
         const user = new User({ name, email, password })
         await user.save()
-        res.json(user)
+        res.status(201).json({ message: 'The user was created', user })
+    
     } catch (err) {
         res.json(err)
         next(err)
@@ -38,12 +47,15 @@ export const updateUser = async (req, res, next) => {
         const { id } = req.params
         const { name, email, password } = req.body
         const currentUser = await User.findById(id)
-
+        if (!currentUser) {
+            res.status(404) 
+            return res.json({ message: 'User not found', currentUser })
+        }
         currentUser.name = name || currentUser.name
         currentUser.email = email || currentUser.email
         currentUser.password = password || currentUser.password
         const user = await currentUser.save()
-        res.json(user)
+        res.status(200).json({ message: 'The user was updated', user })
 
     } catch (err) {
         res.json(err)
@@ -55,7 +67,12 @@ export const deleteUser = async (req, res, next) => {
     try {
         const { id } = req.params
         const user = await User.findByIdAndDelete(id)
-        res.json(user)
+        if (!user) {
+            res.status(404)
+            return res.json({ message: 'User not found', user })
+        }
+        res.status(200).json({ message: 'The user was deleted', user })
+   
     } catch (err) {
         res.json(err)
         next(err)
