5cc2bad13bcab7a805898e43f4df0e99f7055d0a
nestor-labiuk <nestorlabiuk@gmail.com>
2024-03-26 23:17:29 -0300

    Crea un logger para manejo de entidades elimindas

--- src/loggers/index.loggers.js
+++ src/loggers/index.loggers.js
@@ -1,4 +1,5 @@
 import { createLogger, format, transports } from 'winston'
+import DeletedTransport from './transport/DeletedTransport.js'
 
 export const logger = createLogger({
     level: 'info',
@@ -13,6 +14,29 @@ export const logger = createLogger({
         new transports.File({ filename: 'errors.log', level: 'error' }),
         new transports.File({ filename: 'warns.log', level: 'warn' }),
         new transports.File({ filename: 'general.log' }),
+        new transports.Console({
+            format:
+            format.combine(
+                format.colorize(),
+                format.cli()
+            )
+        })
+    ]
+})
+
+export const removedEntitiesLogger = createLogger({
+    level: 'info',
+    format:
+    format.combine(
+        format.json(),
+        format.timestamp({ format: 'DD.MM.YY HH:mm:ss' }),
+        format.prettyPrint()
+    ),
+    defaultMeta: { service: 'user_service' },
+    transports: [
+        new transports.File({ filename: 'removeEntities.log' }),
+        new transports.File({ filename: 'general.log' }),
+        new DeletedTransport({ level: 'info' }),
         new transports.Console({
             format:
             format.combine(
