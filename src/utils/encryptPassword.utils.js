228beddc96cd9c1d766ea5e96bf1d12399983d9b
nestor-labiuk <nestorlabiuk@gmail.com>
2024-04-09 19:47:35 -0300

    Crea función para encriptar contraseñas

---
+++ src/utils/encryptPassword.utils.js
@@ -0,0 +1,7 @@
+import bcrypt from 'bcrypt'
+
+export const encryptPassword = (password) => {
+    const salt = bcrypt.genSaltSync()
+    const hash = bcrypt.hashSync(password, salt)
+    return hash
+}
