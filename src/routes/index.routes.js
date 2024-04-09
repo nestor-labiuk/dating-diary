0f76a72d2b8c378b7e5ac54b53bd780094d25dd4
nestor-labiuk <nestorlabiuk@gmail.com>
2024-04-09 19:49:53 -0300

    agrega ruta de admin al index de rutas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,3 +1,4 @@
 export { default as usersRoutes } from './users.routes.js'
 export { default as loginRoutes } from './login.routes.js'
 export { default as booksRoutes } from './books.routes.js'
+export { default as adminRoutes } from './admin.routes.js'
